50057cae3af9e56375e73c6115a12a7f31d7361c
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-07-08 16:00:19 -0700

    Completed

--- pix/src/App.js
+++ pix/src/App.js
@@ -2,53 +2,102 @@ import React, { Component } from 'react'
 import Card from './Components/Card'
 import Header from './Components/Header'
 
-const cards = [{
-  img: 1,
-  title: 1
-}, {
-  img: 2,
-  title: 2
-}, {
-  img: 3,
-  title: 3
-}, {
-  img: 4,
-  title: 4
-}, {
-  img: 5,
-  title: 5
-}, {
-  img: 6,
-  title: 6
-}]
-
 class App extends Component {
   state = {
     userScore: 0,
-    topScore: 0
+    topScore: 0,
+    clicked: [],
+    cardNums: [1, 2, 3, 4, 5, 6],
+    cards: [{
+      img: 1,
+      title: 1
+    }, {
+      img: 2,
+      title: 2
+    }, {
+      img: 3,
+      title: 3
+    }, {
+      img: 4,
+      title: 4
+    }, {
+      img: 5,
+      title: 5
+    }, {
+      img: 6,
+      title: 6
+    }]
   }
 
-  onClick = () => {
+  onClick = e => {
+    if ((this.state.userScore >= 5) && (this.state.cardNums.indexOf(parseInt(e.target.id)) >= 0) && (this.state.clicked.indexOf(parseInt(e.target.id)) < 0)) {
+
+      let score = this.state.userScore
+      score += 1
+      this.setState({ userScore: score, userScore: score })
+
+    } else if ((this.state.userScore <= 5) && (this.state.cardNums.indexOf(parseInt(e.target.id)) >= 0) && this.state.clicked.indexOf(parseInt(e.target.id)) < 0) {
+
+      console.log('help')
+
+      let clicked = this.state.clicked
+      clicked.push(parseInt(e.target.id))
+      this.setState({ clicked })
+
+      let newCardsArr = this.state.cards
+      for (let i = (newCardsArr.length - 1); i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let x = newCardsArr[i]
+        newCardsArr[i] = newCardsArr[j]
+        newCardsArr[j] = x
+      }
+
+      let score = this.state.userScore
+      score += 1
+
+      this.setState({ cards: newCardsArr, userScore: score })
+
+    } else if ((this.state.cardNums.indexOf(parseInt(e.target.id)) >= 0) && (this.state.clicked.indexOf(parseInt(e.target.id)) >= 0)) {
+      let score = this.state.userScore
+
+      this.setState({ clicked: [], topScore: score, userScore: 0 })
+    }
 
   }
 
-  onFail = () => {
+  onWin = e => {
 
   }
 
-  updateCard = () => {
+  onFail = e => {
+
+
 
   }
 
-  shuffleCards = () => {
+  updateCard = e => {
+    let clicked = this.state.clicked
+    clicked.push(parseInt(e.target.id))
+    this.setState({ clicked })
+    this.shuffleCards()
+  }
 
+  shuffleCards = e => {
+    let newCardsArr = this.state.cards
+    for (let i = (newCardsArr.length - 1); i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1))
+      let x = newCardsArr[i]
+      newCardsArr[i] = newCardsArr[j]
+      newCardsArr[j] = x
+    }
+    this.setState({ cards: newCardsArr })
   }
 
   render() {
     return (
       <>
         <Header userScore={this.state.userScore} topScore={this.state.topScore} />
-        <Card cards={cards} onClick={this.onClick} onFail={this.onFail} updateCard={this.updateCard} shuffleCards={this.shuffleCards} />
+        <Card cards={this.state.cards} onClick={this.onClick} />
       </>
     )
   }
--- pix/src/Components/Card/Card.js
+++ pix/src/Components/Card/Card.js
@@ -1,56 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
+import React from 'react'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
 const Card = props => {
   // console.log(props.cards)
-  const classes = useStyles();
+  console.log(props)
+
   return (
     <>
-
-
-      <div className={classes.root}>
-        <GridList cellHeight={180} className={classes.gridList}>
-          <GridListTile key='Subheader' cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component='div'>December</ListSubheader>
-          </GridListTile>
-          {props.cards.map(card => (
-            <GridListTile key={card.img}>
-              <img src={card.img} alt={card.title} />
-              <GridListTileBar
-                title={card.title}
-                subtitle={<span>by: {card.author}</span>}
-                actionIcon={
-                  <IconButton aria-label={`info about ${card.title}`} className={classes.icon}>
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+      {
+        props.cards.map(card => (
+          <button onClick={props.onClick} id={card.title}>
+            {card.title}
+          </button>
+        ))
+      }
     </>
   )
 }
